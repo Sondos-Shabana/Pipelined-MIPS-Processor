be61e9bfb703b1f1a16317ec2f8d2a5872b15d60
Sondos Shabana <87226586+Sondos-Shabana@users.noreply.github.com>
2022-08-17 19:42:09 +0200

    making RST asynchronous & CLR synchronous

--- D_Register.v
+++ D_Register.v
@@ -12,24 +12,19 @@ module D_Register (
     output reg [102:0] RegD                                    //RsDR           -->   [30:26]
 );                                                             //RtDR           -->   [25:21]
                                                                //RdDR           -->   [20:16]
-reg [102:0] signal_RegD;                                       //SignImmDR      -->   [15:0]
+                                                               //SignImmDR      -->   [15:0]
 
-always@ (*)
+always@ (posedge CLK_DReg or negedge RST_DReg)
 begin
     if((!RST_DReg) | CLR_DReg)
     begin
-        signal_RegD = 103'b0;
+        RegD <= 103'b0;
     end
     else
     begin
-        //                [102]      [101]     [100]      [99:97]      [96]     [95]    [94:63][62:31][30:26][25:21][20:16]  [15:0]
-        signal_RegD = {RegWriteDR,MemtoRegDR,MemWriteDR,ALUControlDR,ALUSrcDR,RegDstDR,  Op1DR,  Op2DR,  RsDR,  RtDR,  RdDR,SignImmDR};
+        //          [102]      [101]     [100]      [99:97]      [96]     [95]    [94:63][62:31][30:26][25:21][20:16]  [15:0]
+        RegD <= {RegWriteDR,MemtoRegDR,MemWriteDR,ALUControlDR,ALUSrcDR,RegDstDR,  Op1DR,  Op2DR,  RsDR,  RtDR,  RdDR,SignImmDR};
     end
-end
-
-always@ (posedge CLK_DReg)
-begin
-    RegD <= signal_RegD;
 end
 
 endmodule
